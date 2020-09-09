7ae35c2c6906e3abbb1e2927da256b75d51b342b
vovasemkovych <37297861+vovasemkovych@users.noreply.github.com>
2020-09-09 13:41:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './style.css';
 import './menu.css';
 import './animation.css'
